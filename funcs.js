3e626de7a91dc9dd5342efde3f09101bd5552f6e
Elijah Moore <65111992+elijahmoore104@users.noreply.github.com>
2020-12-05 14:20:28 +1100

    FunctionsLocation

---
+++ funcs.js
@@ -0,0 +1,16 @@
+
+
+function alertMsg() {
+    alert();
+}
+
+
+function funcAdd(a,b) {
+    return a + b;
+  }
+
+
+function storeUserData(name) {
+    var x = document.getElementById(name).value;
+    document.getElementById(name).innerHTML = x;
+}
